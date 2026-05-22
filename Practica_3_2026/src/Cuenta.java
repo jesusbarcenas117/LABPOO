d5659925cd70a07529beff163fc46492b7a94882
Jesus Barcenas <barcenas11766@gmail.com>
2026-05-22 00:43:06 -0600

    terminado de los getters y setters junto con el método toString

--- Practica_3_2026/src/Cuenta.java
+++ Practica_3_2026/src/Cuenta.java
@@ -15,12 +15,15 @@ public class Cuenta {
     public double getSaldo() {
         return this.saldo_debito;
     }
+
     public double getDeuda() {
         return this.deuda_credito;
     }
+
     public String getDomicilio() {
         return this.domicilio;
     }
+
     public String getNombre() {
         return this.nombre;
     }
@@ -28,21 +31,45 @@ public class Cuenta {
     //setters
 
     public void setSaldo(double saldo) {
-        if (saldo < 0){
+        if (saldo < 0) {
             System.out.println("Saldo negativo, no es posible de asignar");
         } else {
             this.saldo_debito = saldo;
 
         }
     }
+
     public void setDeuda(double deuda) {
-        if (deuda < 0){
+        if (deuda < 0) {
             System.out.println("Deuda negativo, no es posible de asignar");
         } else {
             this.deuda_credito = deuda;
         }
     }
-    public void setDomicilio(String domicilio){
 
+    public void setDomicilio(String domicilio) {
+        if (domicilio == null || domicilio.isEmpty()) {
+            System.out.println("Domicilio vacio");
+        } else {
+            this.domicilio = domicilio;
+        }
+    }
+
+    public void setNombre(String nombre) {
+        if (nombre == null || nombre.isEmpty()) {
+            System.out.println("Nombre vacio");
+        } else if (!nombre.matches("[a-zA-ZáéíóúÁÉÍÓÚ]+")) {
+            System.out.println("No se permiten caracteres especiales ni numeros");
+        } else {
+            this.nombre = nombre;
+        }
+    }
+
+    //metodo toString, este es para poder imprimir datos de objetos como si fueran String nativos
+
+    @Override
+    public String toString(){
+        return "Cuenta{" +"nombre: " + nombre + ", saldo: "+ saldo_debito +
+                ", deuda: " + deuda_credito + ", domicilio: " + domicilio + "}";
     }
 }
