06c171fa8f51e28f56c346d36fe19c28a14b7ad8
Jesus Barcenas <barcenas11766@gmail.com>
2026-05-24 03:28:24 -0600

    solo le agregue el sistema de puntos que piden los requisitos, y separe membresia por que esa la voy a hacer en otra clase, no puedo ocupar la misma clase que la practica pasada

--- PIA_POO_2026/src/main/java/com/example/pia_poo_2026/model/Cliente.java
+++ PIA_POO_2026/src/main/java/com/example/pia_poo_2026/model/Cliente.java
@@ -7,50 +7,58 @@ public class Cliente implements Serializable {
     private String apellido;
     private String email;
     private String telefono;
-    private String membresia; // basica, premium, vip
+    private int puntos;
 
-    public Cliente(String nombre, String apellido, String email, String telefono, String membresia) {
+    public Cliente(String nombre, String apellido, String email, String telefono, int puntos) {
         this.nombre = nombre;
         this.apellido = apellido;
         this.email = email;
         this.telefono = telefono;
-        this.membresia = membresia;
+        this.puntos = 0;
     }
 
     public String getNombre() {
         return nombre;
     }
+
     public String getApellido() {
         return apellido;
     }
+
     public String getEmail() {
         return email;
     }
+
     public String getTelefono() {
         return telefono;
     }
-    public String getMembresia() {
-        return membresia;
+
+    public int getPuntos() {
+        return puntos;
     }
 
     public void setNombre(String nombre) {
         this.nombre = nombre;
     }
+
     public void setApellido(String apellido) {
         this.apellido = apellido;
     }
+
     public void setEmail(String email) {
         this.email = email;
     }
+
     public void setTelefono(String telefono) {
         this.telefono = telefono;
     }
+
     public void setMembresia(String membresia) {
-        this.membresia = membresia;
+        this.puntos = puntos;
     }
 
     @Override
     public String toString() {
-        return nombre + " " + apellido + " | " + membresia + " | " + email;
+        return nombre + " " + apellido + " | " + email + " | Puntos: " + puntos;
     }
 }
