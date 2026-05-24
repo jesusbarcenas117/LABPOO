08cdb59f37d96800c20fdc2db1083c5f5d2713fa
Jesus Barcenas <barcenas11766@gmail.com>
2026-05-23 20:58:17 -0600

    Mismo molde Cuenta de antes, pero ahora los metodos son un proceso sincronizado manejado por hilos, voy a implementar un generador y un consumidor
    el generador genera tareas que el otro cumple y retira

--- Practica_10_2026/src/Cajero.java
+++ Practica_10_2026/src/Cajero.java
@@ -1,2 +1,22 @@
-public class Cajero {
+public class Cajero extends Thread {
+    private CuentaBanco cuenta;
+    private double cantidad;
+    private String operacion;
+
+    public Cajero(String nombre, CuentaBanco cuenta, double cantidad, String operacion) {
+        super(nombre);
+        this.cuenta = cuenta;
+        this.cantidad = cantidad;
+        this.operacion = operacion;
+    }
+
+    @Override
+    public void run() {
+        System.out.println(getName() + " | Iniciando operacion: " + operacion);
+        if (operacion.equals("retiro")) {
+            cuenta.retirar(cantidad);
+        } else {
+            cuenta.depositar(cantidad);
+        }
+    }
 }
--- Practica_10_2026/src/CuentaBanco.java
+++ Practica_10_2026/src/CuentaBanco.java
@@ -1,2 +1,33 @@
 public class CuentaBanco {
+    private double saldo;
+    private String numeroCuenta;
+
+    public CuentaBanco(String numeroCuenta, double saldoInicial) {
+        this.numeroCuenta = numeroCuenta;
+        this.saldo = saldoInicial;
+    }
+
+    public synchronized void retirar(double cantidad) {
+        if (cantidad > saldo) {
+            System.out.println(Thread.currentThread().getName() +
+                    " | RECHAZADO retiro de " + cantidad + " | Saldo insuficiente: " + saldo);
+            return;
+        }
+        saldo -= cantidad;
+        System.out.println(Thread.currentThread().getName() +
+                " | Retiro: " + cantidad + " | Saldo restante: " + saldo);
+    }
+
+    public synchronized void depositar(double cantidad) {
+        saldo += cantidad;
+        System.out.println(Thread.currentThread().getName() +
+                " | Deposito: " + cantidad + " | Saldo actual: " + saldo);
+    }
+
+    public synchronized double getSaldo() {
+        return saldo;
+    }
+    public String getNumeroCuenta() {
+        return numeroCuenta;
+    }
 }
